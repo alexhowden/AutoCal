72d66f1d0c62fe7f36486963f2fd0bccbc0060ae
Alex Howden <ahowden27@gmail.com>
2025-08-18 03:45:36 -0400

    functionality for viewing and adding calendar events

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from typing import TypedDict, Annotated, Sequence
+from typing import TypedDict, Annotated, Sequence, List, Dict
 from dotenv import load_dotenv
 from langchain_core.messages import BaseMessage, ToolMessage, SystemMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
@@ -9,7 +9,37 @@ from langgraph.prebuilt import ToolNode
 import random
 from langchain_tavily import TavilySearch
 
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+import datetime
+import os.path
+
 load_dotenv()
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+def cal_auth():
+	creds = None
+
+	if os.path.exists('token.json'):
+		creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+	if not creds or not creds.valid:
+		if creds and creds.expired and creds.refresh_token:
+			creds.refresh(Request())
+		else:
+			flow = InstalledAppFlow.from_client_secrets_file(
+				'credentials.json', SCOPES
+			)
+		creds = flow.run_local_server(port=0)
+
+	with open('token.json', 'w') as token:
+		token.write(creds.to_json())
+
+	return creds
 
 class AgentState(TypedDict):
 	messages: Annotated[Sequence[BaseMessage], add_messages]
@@ -30,10 +60,99 @@ def multiply(a: int, b: int):
 def rand_num(a: int, b: int):
 	'''This node returns a random number'''
 	return random.randint(a, b)
+@tool
+def cal_view_events(num_events: int):
+	'''	Prints the start and name of the next (num_events) events on the user's calendar. Default to 10 if no number specified.'''
+	creds = cal_auth()
+
+	try:
+		service = build('calendar', 'v3', credentials=creds)
+
+		now = datetime.datetime.now(tz=datetime.timezone.utc).isoformat()
+
+		events_result = (
+			service.events()
+			.list(
+				calendarId='primary',
+				timeMin=now,
+				maxResults=10,
+				singleEvents=True,
+				orderBy='startTime',
+			)
+			.execute()
+		)
+		events = events_result.get('items', [])
+
+		if not events:
+			return 'No upcoming events found.'
+
+		return events
+
+		# formatted_events = []
+
+		# for event in events:
+		# 	start = event['start'].get('dateTime', event['start'].get('date'))
+		# 	summary = event['summary']
+		# 	formatted_events.append(event)
+
+		# return formatted_events
+
+	except HttpError as error:
+		return f'An error occurred: {error}'
+@tool
+def cal_add_event(summary: str, location: str, description: str, start_time: str, end_time: str, timezone: str, recurrence: List[str], attendees: List[Dict[str, str]]):
+	'''
+	This node creates a google calendar event with the specified information.
+	Clarify for critical fields, otherwise it's ok if you leave certain fields blank.
+	summary: title of the event, critical
+	location: location of the event, not critical
+	description: description of event, leave blank unless user specifies details such as room number, attire, reminders, etc
+	start_time: starting time of event, in the form '2015-05-28T09:00:00-07:00', critical
+	end_time: same deal as start_time, critical
+	timezone: unless specified it's going to be 'America/New_York'
+	recurrence: recurrences, follows the iCalendar (RFC 5545) standard, use when applicable
+	attendees: list of dictionaries of attendees (emails), here's an example: [
+			{'email': 'lpage@example.com'},
+			{'email': 'sbrin@example.com'},
+		], will usually be empty
+	'''
+	creds = cal_auth()
+
+	try:
+		service = build('calendar', 'v3', credentials=creds)
+
+		event = {
+			'summary': summary,
+			'location': location,
+			'description': description,
+			'start': {
+				'dateTime': start_time,
+				'timeZone': timezone,
+			},
+			'end': {
+				'dateTime': end_time,
+				'timeZone': timezone,
+			},
+			'recurrence': recurrence,
+			'attendees': attendees,
+			'reminders': {
+				'useDefault': True,
+				# 'overrides': [
+				# 	{'method': 'email', 'minutes': 24 * 60},
+				# 	{'method': 'popup', 'minutes': 10},
+				# ],
+			},
+		}
+
+		event = service.events().insert(calendarId='primary', body=event).execute()
+		return f'Event created: {event}'
+
+	except HttpError as error:
+		return f'An error occurred: {error}'
 
 tavily_search = TavilySearch()
 
-tools = [add, subtract, multiply, rand_num, tavily_search]
+tools = [add, subtract, multiply, rand_num, tavily_search, cal_view_events, cal_add_event]
 
 llm = ChatGoogleGenerativeAI(
     # model='gemini-2.0-flash-lite',
@@ -42,7 +161,8 @@ llm = ChatGoogleGenerativeAI(
 ).bind_tools(tools)
 
 def model_call(state: AgentState) -> AgentState:
-	system_prompt = SystemMessage(content='You are my AI assistant, please answer my query to the best of your ability.')
+	now = datetime.datetime.now(tz=datetime.timezone.utc).isoformat()
+	system_prompt = SystemMessage(content=f'You are my AI assistant, please answer my query to the best of your ability. For calendar-related inquiries, only clarify on critical fields, otherwise do your best without. They will know what they want. Here is the current date and time as a reference point: {now}')
 	response = llm.invoke([system_prompt] + state['messages'])
 	return {'messages': [response]}
 
